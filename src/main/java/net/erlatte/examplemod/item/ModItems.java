a6dc341ffe135336cb0c69a5a736d615c1455ae2
Erzlatte <146171298+Erzlatte@users.noreply.github.com>
2024-05-11 13:02:04 +0200

    0

---
+++ src/main/java/net/erlatte/examplemod/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.erlatte.examplemod.item;
+
+import net.erlatte.examplemod.ExampleMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+
+public class ModItems
+{
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, ExampleMod.MOD_ID);
+
+    public static final RegistryObject<Item> GUN = ITEMS.register("gun",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
